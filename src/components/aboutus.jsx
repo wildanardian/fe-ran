8c6726040973f4f2154ad55a91e166cbb5a11e96
JorLojor <nugas123man@gmail.com>
2023-08-11 05:13:51 +0700

    frontend inisiation

---
+++ src/components/aboutus.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function AboutusComponent() {
+     return (
+            <>
+                <div className="container-fluid">
+                    <div className="row  container-about-us">
+                            <div className="col-5 p-0 set-aboutus-image"></div>
+
+                            <div className="col-7 set-aboutus-text p-5">
+                                <div className="custom-text">
+                                    <h1>About Us</h1>
+                                    <p>
+                                        Lorem ipsum dolor, sit amet consectetur
+                                        adipisicing elit. Officia maiores quasi
+                                        cumque, sint architecto consequatur ea
+                                        modi aperiam vitae aliquam
+                                        necessitatibus ex eligendi voluptate
+                                        obcaecati optio ducimus magni. Quidem
+                                        corrupti dolore at itaque, distinctio
+                                        praesentium molestiae similique ipsum,
+                                        perspiciatis totam quam! Fugiat
+                                        voluptas, repellendus voluptatum
+                                        excepturi debitis porro quae qui?
+                                    </p>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </>
+    );
+}
+
+export default AboutusComponent;
---
+++ src/components/visimisi.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function VisimisiComponent() {
+    return(
+        <>
+        <div class="container-fluid justify-content-center d-flex">
+            <div class="row cover-set-visimisi text-light">
+                <div class="col-6">
+                    <div class="text-visi-misi">
+                        <h1 class="set-title-visimisi">FISI</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae sint, eveniet voluptatum esse deserunt excepturi quaerat. Eligendi numquam vitae ipsa beatae ut sunt eos incidunt quos sequi, quis iusto consectetur.</p>
+                    </div>
+                    
+                </div>
+                <div class="col-6 mr-1">
+                    <div class="text-visi-misi">
+                        <h1 class="set-title-visimisi">MISI</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae sint, eveniet voluptatum esse deserunt excepturi quaerat. Eligendi numquam vitae ipsa beatae ut sunt eos incidunt quos sequi, quis iusto consectetur.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default VisimisiComponent;
